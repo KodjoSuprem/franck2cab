faf1157140ff1f55767aba4b2741f9a763c21752
KodjoSuprem <kodjosuprem@gmail.com>
2025-07-06 02:39:12 +0200

    fix: remove unnecessary script directory variable for download archive

--- yt_channel_audio.sh
+++ yt_channel_audio.sh
@@ -21,6 +21,5 @@ if [ -z "$1" ]; then
 fi
 
 URL="$1"
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-yt-dlp --no-mtime --download-archive "$SCRIPT_DIR/download_archive.txt" -f bestaudio --embed-metadata --extract-audio --audio-format mp3 --embed-thumbnail --output "%(title)s.%(ext)s" "$URL"
+yt-dlp --no-mtime --download-archive download_archive.txt -f bestaudio --embed-metadata --extract-audio --audio-format mp3 --embed-thumbnail --output "%(title)s.%(ext)s" "$URL"
